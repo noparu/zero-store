f011cad6cf73657ae26f682f3e6674e73e904631
noparu <noparuuu@gmail.com>
2025-01-05 20:57:28 +0700

    create base web

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      gridTemplateColumns: {
+        '19': 'repeat(19, minmax(0, 1fr))'
+      }
     },
   },
   plugins: [],
